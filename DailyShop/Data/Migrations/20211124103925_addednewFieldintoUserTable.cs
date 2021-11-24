21557668fdd4b3df2f37b49c8a262e9c6981abb4
Farhad Hossain <61607180+Fahim-143Muntasir@users.noreply.github.com>
2021-11-24 16:45:33 +0600

    scaffold item(identity) added to the project and a new model called ApplicationUser added with Firstname and last name prop

---
+++ DailyShop/Data/Migrations/20211124103925_addednewFieldintoUserTable.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DailyShop.Data.Migrations
+{
+    public partial class addednewFieldintoUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypesId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductTypesId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductTypesId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductTypesId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductTypesId",
+                table: "Products",
+                column: "ProductTypesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypesId",
+                table: "Products",
+                column: "ProductTypesId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
